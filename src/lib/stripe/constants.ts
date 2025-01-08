7adf86c43333a95481b8eca552858cf8d2aa7b49
Sprint Platform <noreply@sprint.dev>
2025-01-08 23:54:44 +0900

    Add src/lib/stripe/constants.ts

---
+++ src/lib/stripe/constants.ts
@@ -0,0 +1,7 @@
+export const STRIPE = {
+  PRICES: {
+    CREDITS_100: 'price_1Qd30jBVl8JU2CFUJSrFDA6i'
+  },
+  PACKAGE_SIZE: 100, // Credits per package
+  PRICE_PER_PACKAGE: 1 // USD
+} as const;
